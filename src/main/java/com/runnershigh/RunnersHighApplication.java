60b6210301c35cd17d26ee586157aa06038dd804
Jung-HyeonSu <gustn05233@gmail.com>
2026-02-07 15:24:50 +0900

    init project

---
+++ src/main/java/com/runnershigh/RunnersHighApplication.java
@@ -0,0 +1,13 @@
+package com.runnershigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersHighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersHighApplication.class, args);
+	}
+
+}
